5045823b76414dd23dc3deb894e104cda7b122fe
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-02-08 21:47:18 +0200

    task1-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,26 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const imgContainer = document.querySelector('div.gallery');
+const cardMarkup = creteImgCard(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', cardMarkup)
+
+function creteImgCard(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+      <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
+        <img class="gallery__image" src="${preview}" alt="${description}" title = "${description}" />
+      </a>
+    </div>`;
+  }).join('');
+};
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom',  });
