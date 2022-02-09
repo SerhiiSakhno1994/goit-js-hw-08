01f5a67f311d23927c8324c9bcf7e816cc42a895
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-02-09 09:50:25 +0200

    updata-task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,31 @@
 import throttle from 'lodash.throttle';
 
+
+
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onFormInput(e) {
+ 
+}
+
+function fillInTextArea() {
+  
+}
+
+
+
+
+
