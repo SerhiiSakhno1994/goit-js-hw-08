78ff18954827df131aa84140f04c525bb69116b9
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-02-09 09:22:53 +0200

    task2-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(playerCurrentTime, 1000));
+player.on('ended', endVideo);
+setVideoTime();
+
+function playerCurrentTime(data) {
+  const videoTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', videoTime);
+  
+}
+
+function endVideo() {
+  player.off('timeupdate');
+  localStorage.removeItem('videoplayer-current-time');
+}
+
+function setVideoTime() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
